2dbec9f8d1232d2b67b1d174daa8ff8eb819790d
Stijn Vergauwen <stijnvergauwen@gmail.com>
2023-05-27 15:21:31 +0200

    make functionality to create new item through cli

--- src/items.rs
+++ src/items.rs
@@ -1,31 +1,64 @@
-pub mod items {
-    use crate::Category;
+// use crate::Category;
+use rand::Rng;
 
-    pub struct Item {
-        name: String,
-        quantity: u32,
-        price: f64,
-        category: Category,
-        sku: u32,
+use crate::await_user_input;
+
+#[derive(Debug)]
+pub struct Item {
+    name: String,
+    quantity: u32,
+    price: f64,
+    sku: u32,
+}
+
+impl Item {
+    pub fn create_new(name: String, price: f64) -> Item {
+        let mut rng = rand::thread_rng();
+
+        Item {
+            name,
+            price,
+            quantity: 0,
+            sku: rng.gen_range(10_000..99_999),
+        }
+    }
+}
+
+pub fn handle_create_new_item() -> Item {
+    println!("Enter the name of the new item");
+
+    let item_name: String = await_user_input();
+
+    println!("Enter the item's price");
+
+    let item_price: f64 = await_user_input()
+        .parse()
+        .expect("User's price input should be a number");
+
+    return Item::create_new(item_name, item_price);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_create_item() {
+        let item = Item {
+            name: String::from("test item"),
+            quantity: 4,
+            price: 100.0,
+            sku: 20,
+        };
+
+        assert_eq!(item.name, "test item");
+        assert_eq!(item.price, 100.0);
     }
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
+    #[test]
+    fn has_5_digit_sku() {
+        let item = Item::create_new(String::from("test item"), 80.0);
 
-        #[test]
-        fn can_create_item() {
-            let item = Item {
-                name: String::from("test item"),
-                quantity: 4,
-                price: 100.0,
-                category: Category {
-                    name: String::from("default"),
-                },
-                sku: 20,
-            };
-
-            assert_eq!(item.name, "test item");
-        }
+        assert!(item.sku > 9000 && item.sku < 100_000);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,50 @@
+use crate::items::{handle_create_new_item, Item};
+use std::io;
+
 mod items;
 
-struct Category {
-    name: String,
-}
+// struct Category {
+//     name: String,
+// }
 
 fn main() {
-    println!("Hello, world!");
+    let item: Item = handle_create_new_item();
+
+    println!("Created item has following data:");
+
+    println!("{:?}", item);
+}
+
+fn await_user_choice(message: String, choices: &Vec<String>) -> i32 {
+    println!("{message}");
+
+    for (index, choice) in choices.iter().enumerate() {
+        println!("{}. {}", index + 1, choice);
+    }
+
+    let mut selected_number: i32;
+
+    loop {
+        selected_number = match await_user_input().parse() {
+            Ok(number) => number,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
+
+        if selected_number > 0 && selected_number < choices.len().try_into().unwrap() {
+            return selected_number;
+        }
+    }
+}
+
+fn await_user_input() -> String {
+    let mut user_input = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("User input should be valid");
+
+    return user_input.trim().to_string();
 }
